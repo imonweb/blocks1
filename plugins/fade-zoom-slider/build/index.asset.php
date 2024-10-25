e77e6d4268154a1ddd906ce8c1d0bbc4295a947a
Imon <contact@imonweb.co.uk>
2024-10-26 00:27:54 +0100

    fade-zoom-slider plugin

--- plugins/fade-zoom-slider/build/index.asset.php
+++ plugins/fade-zoom-slider/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3a3bbe1b9f17fdf779ff');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bdf13a6b17ec9c0194c5');
